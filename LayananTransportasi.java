0bb44f33aa9f2306b5ef63555a2e690df3561bb6
Artemisee77 <a123z4i6z@gmail.com>
2026-04-14 14:05:42 +0700

    init project dan parent class LayananTransportasi

---
+++ LayananTransportasi.java
@@ -0,0 +1,21 @@
+public abstract class LayananTransportasi {
+    protected String namaDriver;
+    protected double jarak;
+    protected double saldoUser;
+
+    public LayananTransportasi(String namaDriver, double jarak, double saldoUser) {
+        this.namaDriver = namaDriver;
+        this.saldoUser = saldoUser;
+
+        if (jarak <= 0) {
+            this.jarak = 1.0;
+        } else {
+            this.jarak = jarak;
+        }
+    } 
+
+public abstract double hitungTarif();
+
+public abstract void prosesTransaksi();
+
+}
